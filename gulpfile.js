85b0f1e72de78a5b427db7180743092e6bc31bd8
Andrew Reilley <reilleya@mit.edu>
2020-05-02 23:28:01 -0400

    Add 'clean', 'less', and 'build' commands to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 var gulp = require('gulp');  
 var pug = require('gulp-pug');
+var less = require('gulp-less');
+var del = require('del');
 
-gulp.task('pug', function() {  
-  return gulp.src('templates/*.pug')
+clean = function () {
+    return del([
+        './build/**/*',
+      ]);
+}
+
+buildPug = function () {
+    return gulp.src('templates/*.pug')
       .pipe(pug())
       .pipe(gulp.dest('build'));
-});
+  };
+
+buildLess = function () {
+    return gulp.src('./templates/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./build/styles'));
+}
+
+gulp.task('clean', clean);
+
+gulp.task('pug', buildPug);
+
+gulp.task('less', buildLess);
+
+gulp.task('build', gulp.series(clean, buildPug, buildLess));
